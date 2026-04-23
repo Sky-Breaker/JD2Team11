2e741b79f2ecd28cf0216126a873164f10ae1c9f
Leonard Dattilo <ffpilot22@outlook.com>
2026-04-22 22:27:47 -0700

    Fix return types in script_helpers

--- PythonUI/scripts/script_helpers.py
+++ PythonUI/scripts/script_helpers.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 REQUIREMENTS_FILENAME = 'requirements.txt'
 
-def get_project_root_path():
+def get_project_root_path() -> Path:
     # __file__ is an attribute that gives the absolute path to this script file
     scripts_path = Path(__file__)
 
@@ -14,22 +14,22 @@ def get_project_root_path():
 
     return project_root_path
 
-def _get_python_path(use_venv: bool):
+def _get_python_path(use_venv: bool) -> str:
     project_root_path = get_project_root_path()
     venv_exists = (project_root_path / '.venv').exists()
     if (use_venv and venv_exists):
         # Find correct venv python path for OS
         if (os.name == "nt"):
             # Windows venv python path
-            return project_root_path / '.venv' / 'Scripts' / 'python.exe'
+            return str(project_root_path / '.venv' / 'Scripts' / 'python.exe')
         else:
             # Other system venv python path
-            return project_root_path / '.venv' / 'bin'/ 'python'
+            return str(project_root_path / '.venv' / 'bin'/ 'python')
     else:
         return 'python'
 
 def run_command(args: list[str], capture_output=False, use_venv: bool=True):
-    python = str(_get_python_path(use_venv))
+    python = _get_python_path(use_venv)
     print('Running:', python + ' -m ' + " ".join(args))
     # Run a terminal command using subprocess library
     result = subprocess.run(
